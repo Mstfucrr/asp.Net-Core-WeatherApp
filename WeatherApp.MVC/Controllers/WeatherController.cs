3cabb9b01cf2fe5370fad41b3120430d6caf2b8c
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-04-09 05:09:02 +0300

    Proje dosyası ekle.

---
+++ WeatherApp.MVC/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.MVC.Controllers
+{
+	public class WeatherController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+	}
+}
