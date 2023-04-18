08442e24f26d94a5fe18c2c31d20c9645ea5cd71
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-04-18 22:13:27 +0300

    add null check

--- WeatherApp.MVC/Controllers/WeatherController.cs
+++ WeatherApp.MVC/Controllers/WeatherController.cs
@@ -6,33 +6,35 @@ namespace WeatherApp.MVC.Controllers
 {
 	public class WeatherController : Controller
 	{
-		private WeatherOptions weatherOptions;
-		private WeatherService weatherService;
+		private readonly WeatherOptions _weatherOptions;
+		private readonly WeatherService _weatherService;
+		private WeatherApiResponse? _weatherApiResponse;
 		public WeatherController()
 		{
-			weatherOptions = new WeatherOptions
+			_weatherOptions = new WeatherOptions
 			{
 				ApiKey = "d1398cba8a894feb9f7180821232602"
 			};
-
-			weatherService = new WeatherService(weatherOptions);
+			_weatherApiResponse = new WeatherApiResponse();
+			_weatherService = new WeatherService(_weatherOptions);
 		}
 
         [HttpGet]
         public async Task<IActionResult> Index()
 		{
-			var weatherStatistic = await weatherService.GetWeatherAsync("London");
+			_weatherApiResponse = await _weatherService.GetWeatherAsync("London");
 
-			if (weatherStatistic != null) ViewBag.WeatherResponse = weatherStatistic;
+			if (_weatherApiResponse != null) ViewBag.WeatherResponse = _weatherApiResponse;
 			return View();
 		}
 
         [HttpPost]
         public async Task<IActionResult> Index(Weather weather)
         {
-	        var weatherStatistic = await weatherService.GetWeatherAsync(weather.CityName);
+	        _weatherApiResponse = await _weatherService.GetWeatherAsync(weather.CityName);
 
-	        if (weatherStatistic != null) ViewBag.WeatherResponse = weatherStatistic;
+	        if (_weatherApiResponse != null) ViewBag.WeatherResponse = _weatherApiResponse;
+	        
 	        return View();
         }
     }
